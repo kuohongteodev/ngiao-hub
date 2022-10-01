8e288a2b007a96d60fb2d5dc5c5f149a5265b7c2
kuohongteodev <kuohongteodev@gmail.com>
2022-10-01 16:55:01 +0800

    Initial Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/layout/default";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
